ae0d820effae0dfbe6140bc9c31a86175de24080
psteinhoff <psteinhoff@eonerc.rwth-aachen.de>
2014-07-22 22:43:23 +0200

    Initial commit of mvs2010 projects for creating a dll

---
+++ mvs2010/Liboptv_test/My Project/AssemblyInfo.vb
@@ -0,0 +1,37 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Liboptv_test")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("E.ON Energy Research Center")> 
+<Assembly: AssemblyProduct("Liboptv_test")> 
+<Assembly: AssemblyCopyright("Copyright © E.ON Energy Research Center 2014")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: CLSCompliant(True)> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("da7a41a0-9c39-49a9-bb85-2bb84eb03719")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
---
+++ mvs2010/Liboptv_test/Test_imageprocessing.vb
@@ -0,0 +1,11 @@
+﻿Imports System.Text
+
+<TestClass()>
+Public Class Test_imageprocessing
+    Declare Sub liboptv Lib "D:\OpenPTV\openptv-tcl\mvs2010\Debug\liboptv_imageprocessing.dll" ()
+
+    <TestMethod()>
+    Public Sub TestMethod1()
+    End Sub
+
+End Class
